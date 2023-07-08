4cc2b4be4d8a8939c0ddff4d597be09566e77f65
Adifatek <ahmadifatek@gmail.com>
2023-07-08 21:35:40 +0800

    Add Menu surat masuk + crud

--- application/controllers/admin/Menu.php
+++ application/controllers/admin/Menu.php
@@ -7,6 +7,8 @@ class Menu extends CI_Controller {
     {
         parent::__construct();
         // $this->load->model("product_model"); 
+        cek_login();
+		cek_user();	
         $this->load->library('form_validation');
     }
 	public function index()
@@ -14,8 +16,7 @@ class Menu extends CI_Controller {
 		$data = array(
 			'title' => 'Dashboard',
 			'content' => "admin/dashboard/index"
-		);
-		
+		);		
 		$this->load->view('admin/template/main',$data);
 	}
 }
